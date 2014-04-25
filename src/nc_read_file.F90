4fcc9f3a305d179de88a2e869938a09e9159d46e
Greg McGarragh <gregory.mcgarragh@physics.ox.ac.uk>
2014-04-25 06:21:26 +0000

    Added nc_read_array_2d_float_to_float_orac() and nc_read_array_2d_float_to_float_orac2().

--- src/nc_read_file.F90
+++ src/nc_read_file.F90
@@ -21,6 +21,9 @@
 !2012/02/09 Matthias Jerg adds routines for ORAC.
 !2012/07/06 MJ extensively overhauls and restructures the code
 !2012/08/21 MJ builds this code collection based on pre- and postprocessing code collections
+!2014/04/20, Greg McGarragh: Added nc_read_array_2d_float_to_float_orac() and
+!                                  nc_read_array_2d_float_to_float_orac2().
+!
 ! $Id$
 !
 ! Bugs:
@@ -122,6 +125,7 @@ SUBROUTINE nc_read_array_1d_int_to_int_orac(ncid,n1,cv,v,wo)
    END SUBROUTINE nc_read_array_1d_int_to_int_orac
 
 
+
 !-----------------------------------------------------------------------------
 !-----------------------------------------------------------------------------
 SUBROUTINE nc_read_array_1d_float_to_float_orac(ncid,n1,cv,v,wo)
@@ -214,6 +218,301 @@ SUBROUTINE nc_read_array_1d_float_to_float_orac(ncid,n1,cv,v,wo)
       
 
    END SUBROUTINE nc_read_array_1d_float_to_float_orac
+
+
+
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+SUBROUTINE nc_read_array_2d_float_to_float_orac(ncid,n1,n2,cv,v,wo)
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+  ! Description:
+  !
+  ! Creates new netcdf-file. (Output file)
+  !
+  !-----------------------------------------------------------------------
+  ! This software was developed within the ESA Cloud CCI Project
+  ! and is based on routines developed during the
+  ! ESA DUE GlobVapour Project. 
+  ! Copyright 2011, DWD, All Rights Reserved.
+  !-----------------------------------------------------------------------
+  !
+  ! Unit Name:           nc_create_global_l2.f90
+  !
+  ! Created on:          05/12/10
+  !                      by Matthias Jerg, DWD/KU22
+  !                      (matthias.jerg@dwd.de)
+  !                      based on code provided by Nadine Schneider (nadine.schneider@dwd.de).
+  !
+  !   
+  ! Modifications Log:    
+  !
+  ! Applied SPRs:
+  !
+  !-----------------------------------------------------------------------
+  !
+  ! Declarations:
+  !
+  !---------------------------------
+
+  USE netcdf
+
+  USE ECP_Constants
+
+   IMPLICIT NONE
+
+!   INCLUDE 'netcdf.inc'
+
+      ! Input
+      INTEGER,INTENT(IN) :: ncid,n1,n2,wo           ! number of footprints
+      CHARACTER(LEN=*) :: cv                     ! variable name
+
+      ! Output
+      INTEGER, PARAMETER :: SINGLE = 4
+      INTEGER, PARAMETER :: DOUBLE = 8
+
+      ! Local
+      INTEGER :: ierr, vid, start(2), counter(2),stride(2)
+      REAL,PARAMETER :: miss=-9999.
+      real(kind=sreal) :: v(1:n1,1:n2),fv
+
+   ! End of header ----------------------------------------------------------
+
+!in fortran the inner loop is next to variable
+!e.g. var(n2,n1) n2 is inner loop, n1 is outer loop
+
+      start = 1
+      counter(1) = n2
+      counter(2) = n1
+      stride=1
+
+      ierr = 0
+
+      IF (wo.EQ.1) THEN
+         write(*,*) 'read variable: ', cv
+      ENDIF
+
+      ierr = NF90_INQ_VARID(ncid, cv, vid)    !get variable ID from variable named cv
+      IF (ierr.NE.NF90_NOERR) THEN
+          STOP 'inq v'
+      ENDIF
+
+      ierr = NF90_GET_VAR(ncid, vid,v,start,counter,stride)   !read variable from1-n1 and put into field
+      IF (ierr.NE.NF90_NOERR) THEN
+         STOP 'get v'
+      ENDIF
+
+      ierr = NF90_GET_ATT(ncid, vid, '_FillValue', fv)
+      IF (wo.EQ.1) THEN
+         write(*,*) '_FillVallue: ',fv
+      ENDIF
+      IF (ierr.NE.NF90_NOERR) THEN
+         IF (wo.EQ.1) THEN
+            write(*,*) '_FillVallue: not defined '
+         ENDIF
+      ENDIF
+      
+
+   END SUBROUTINE nc_read_array_2d_float_to_float_orac
+
+
+
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+SUBROUTINE nc_read_array_2d_float_to_float_orac2(ncid,n1,n2,i3,cv,v,wo)
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+  ! Description:
+  !
+  ! Creates new netcdf-file. (Output file)
+  !
+  !-----------------------------------------------------------------------
+  ! This software was developed within the ESA Cloud CCI Project
+  ! and is based on routines developed during the
+  ! ESA DUE GlobVapour Project. 
+  ! Copyright 2011, DWD, All Rights Reserved.
+  !-----------------------------------------------------------------------
+  !
+  ! Unit Name:           nc_create_global_l2.f90
+  !
+  ! Created on:          05/12/10
+  !                      by Matthias Jerg, DWD/KU22
+  !                      (matthias.jerg@dwd.de)
+  !                      based on code provided by Nadine Schneider (nadine.schneider@dwd.de).
+  !
+  !   
+  ! Modifications Log:    
+  !
+  ! Applied SPRs:
+  !
+  !-----------------------------------------------------------------------
+  !
+  ! Declarations:
+  !
+  !---------------------------------
+
+  USE netcdf
+
+  USE ECP_Constants
+
+   IMPLICIT NONE
+
+!   INCLUDE 'netcdf.inc'
+
+      ! Input
+      INTEGER,INTENT(IN) :: ncid,n1,n2,i3,wo           ! number of footprints
+      CHARACTER(LEN=*) :: cv                     ! variable name
+
+      ! Output
+      INTEGER, PARAMETER :: SINGLE = 4
+      INTEGER, PARAMETER :: DOUBLE = 8
+
+      ! Local
+      INTEGER :: ierr, vid, start(3),counter(3),stride(3)
+      REAL,PARAMETER :: miss=-9999.
+      real(kind=sreal) :: v(1:n1,1:n2),fv
+
+   ! End of header ----------------------------------------------------------
+
+!in fortran the inner loop is next to variable
+!e.g. var(n2,n1) n2 is inner loop, n1 is outer loop
+
+      start(1) = i3
+      start(2) = 1
+      start(3) = 1
+
+      counter(1) = 1
+      counter(2) = n2
+      counter(3) = n1
+
+      stride=1
+
+      ierr = 0
+
+      IF (wo.EQ.1) THEN
+         write(*,*) 'read variable: ', cv
+      ENDIF
+
+      ierr = NF90_INQ_VARID(ncid, cv, vid)    !get variable ID from variable named cv
+      IF (ierr.NE.NF90_NOERR) THEN
+          STOP 'inq v'
+      ENDIF
+
+      ierr = NF90_GET_VAR(ncid, vid,v,start,counter,stride)   !read variable from1-n1 and put into field
+      IF (ierr.NE.NF90_NOERR) THEN
+         STOP 'get v'
+      ENDIF
+
+      ierr = NF90_GET_ATT(ncid, vid, '_FillValue', fv)
+      IF (wo.EQ.1) THEN
+         write(*,*) '_FillVallue: ',fv
+      ENDIF
+      IF (ierr.NE.NF90_NOERR) THEN
+         IF (wo.EQ.1) THEN
+            write(*,*) '_FillVallue: not defined '
+         ENDIF
+      ENDIF
+      
+
+   END SUBROUTINE nc_read_array_2d_float_to_float_orac2
+
+
+
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+SUBROUTINE nc_read_array_3d_float_to_float_orac(ncid,n1,n2,n3,cv,v,wo)
+!-----------------------------------------------------------------------------
+!-----------------------------------------------------------------------------
+  ! Description:
+  !
+  ! Creates new netcdf-file. (Output file)
+  !
+  !-----------------------------------------------------------------------
+  ! This software was developed within the ESA Cloud CCI Project
+  ! and is based on routines developed during the
+  ! ESA DUE GlobVapour Project. 
+  ! Copyright 2011, DWD, All Rights Reserved.
+  !-----------------------------------------------------------------------
+  !
+  ! Unit Name:           nc_create_global_l2.f90
+  !
+  ! Created on:          05/12/10
+  !                      by Matthias Jerg, DWD/KU22
+  !                      (matthias.jerg@dwd.de)
+  !                      based on code provided by Nadine Schneider (nadine.schneider@dwd.de).
+  !
+  !   
+  ! Modifications Log:    
+  !
+  ! Applied SPRs:
+  !
+  !-----------------------------------------------------------------------
+  !
+  ! Declarations:
+  !
+  !---------------------------------
+
+  USE netcdf
+
+  USE ECP_Constants
+
+   IMPLICIT NONE
+
+!   INCLUDE 'netcdf.inc'
+
+      ! Input
+      INTEGER,INTENT(IN) :: ncid,n1,n2,n3,wo           ! number of footprints
+      CHARACTER(LEN=*) :: cv                     ! variable name
+
+      ! Output
+      INTEGER, PARAMETER :: SINGLE = 4
+      INTEGER, PARAMETER :: DOUBLE = 8
+
+      ! Local
+      INTEGER :: ierr, vid, start(3), counter(3),stride(3)
+      REAL,PARAMETER :: miss=-9999.
+      real(kind=sreal) :: v(1:n1,1:n2,1:n3),fv
+
+   ! End of header ----------------------------------------------------------
+
+!in fortran the inner loop is next to variable
+!e.g. var(n2,n1) n2 is inner loop, n1 is outer loop
+
+      start = 1
+      counter(1) = n3
+      counter(2) = n2
+      counter(3) = n1
+      stride=1
+
+      ierr = 0
+
+      IF (wo.EQ.1) THEN
+         write(*,*) 'read variable: ', cv
+      ENDIF
+
+      ierr = NF90_INQ_VARID(ncid, cv, vid)    !get variable ID from variable named cv
+      IF (ierr.NE.NF90_NOERR) THEN
+          STOP 'inq v'
+      ENDIF
+
+      ierr = NF90_GET_VAR(ncid, vid,v,start,counter,stride)   !read variable from1-n1 and put into field
+      IF (ierr.NE.NF90_NOERR) THEN
+         STOP 'get v'
+      ENDIF
+
+      ierr = NF90_GET_ATT(ncid, vid, '_FillValue', fv)
+      IF (wo.EQ.1) THEN
+         write(*,*) '_FillVallue: ',fv
+      ENDIF
+      IF (ierr.NE.NF90_NOERR) THEN
+         IF (wo.EQ.1) THEN
+            write(*,*) '_FillVallue: not defined '
+         ENDIF
+      ENDIF
+      
+
+   END SUBROUTINE nc_read_array_3d_float_to_float_orac
+
 
 
 SUBROUTINE nc_read_array_3d_float_orac(ncid,n1,n2,ichan,cv,v,wo)
@@ -315,6 +614,8 @@ SUBROUTINE nc_read_array_3d_float_orac(ncid,n1,n2,ichan,cv,v,wo)
       ENDIF
 
     END SUBROUTINE nc_read_array_3d_float_orac
+
+
 
 SUBROUTINE nc_read_array_1p1_float_orac(ncid,n1,ichan,cv,v,wo)
   ! Description:
@@ -547,8 +848,6 @@ SUBROUTINE nc_read_array_1p2_float_orac(ncid,n1,ichan,ilay,cv,v,wo)
       ENDIF
 
     END SUBROUTINE nc_read_array_1p2_float_orac
-
-
 
 
 
@@ -867,6 +1166,8 @@ SUBROUTINE nc_read_array_2d_byte_to_real_orac(ncid,n1,n2,cv,v_out,wo)
 
    END SUBROUTINE nc_read_array_2d_byte_to_real_orac
 
+
+
 !-----------------------------------------------------------------------------
 !-----------------------------------------------------------------------------
 SUBROUTINE nc_read_array_2d_int_to_real_orac(ncid,n1,n2,cv,v_out,wo)
@@ -981,6 +1282,7 @@ SUBROUTINE nc_read_array_2d_int_to_real_orac(ncid,n1,n2,cv,v_out,wo)
 
 
     END SUBROUTINE nc_read_array_2d_int_to_real_orac
+
 
 
 !-----------------------------------------------------------------------------
