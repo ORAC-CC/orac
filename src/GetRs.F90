6309493947a60473929451df98b50b23462614b3
Greg McGarragh <gregory.mcgarragh@physics.ox.ac.uk>
2014-07-30 09:06:38 +0000

    Some code cleanups.

--- src/GetRs.F90
+++ src/GetRs.F90
@@ -1,103 +1,105 @@
+!-------------------------------------------------------------------------------
 ! Name:
 !    Get_Rs
 !
 ! Purpose:
 !    Calculates the super pixel mean surface reflectivities (and covariances)
-!    for the solar channels according to the specified averaging method Ctrl%Resoln%Ameth
+!    for the solar channels according to the specified averaging method
+!    Ctrl%Resoln%Ameth
 !
 ! Arguments:
-!    Name        Type           In/Out   Description
-!    Ctrl        struct         In       Control structure
-!    SPixel      alloc struct   Both     Super pixel structure
-!    SPixel_b    real array     In       Super pixel array of b (using super pixel surface flags)
-!    SPixel_Sb   real array     In       Super pixel array of Sb (using super pixel surface flags)   
-!    status      integer        Out      Error status
-!    
+!    Name      Type         In/Out Description
+!    Ctrl      struct       In     Control structure
+!    SPixel    alloc struct Both   Super pixel structure
+!    SPixel_b  real array   In     Super pixel array of b (using super pixel
+!                                  surface flags)
+!    SPixel_Sb real array   In     Super pixel array of Sb (using super pixel
+!                                  surface flags)
+!    status    integer      Out    Error status
+!
 ! Algorithm:
 !    Average method:
 !       'All' - average super pixel surface reflectance array values (and covariances)
-!               over all good pixels,            
+!               over all good pixels,
 !       'Cloudy' - average over good cloudy pixels,
 !       'Central' - take values from the central pixel, if it is good.
 !
 ! Local variables:
-!    Name      Type   Description
-!    i, j      int    Loop counters
-!    message   char   Message to write to log file
+!    Name Type Description
 !
 ! History:
-!   4th December, 2000, Kevin M. Smith : original version
-!  16th January,  2001,  KMS : Re-drafted
-!  18th January,  2001,  KMS : Modified to take array of reflectances etc from SPixel%Surface
-!                            : Changed name from Get_Surface_Rs to Get_Rs
-!   16th Mar 2001, Andy Smith:
-!      Removed checking for invalid averaging method. 
-!      Using named constants for averaging method.
-!   15th Jun 2001, Andy Smith:
-!      Changed error message string assignments/writes. 
-!      Long message strings were wrapped over two lines with only one set of 
-!      quotes around the whole string (including the line continuation marker). 
-!      Original code works on DEC but not Linux.
-!   *************************** ECV work starts here *********************
-!   23rd Feb 2011, Andy Smith:
-!      Cloud flags converted to real to match current ORAC data. 
-!   30th Mar 2011, Andy Smith:
-!      Removal of super-pixel averaging. Process single pixels at X0, Y0, 
-!      removed other SPixel indices Xc, Yc, Xn, Yn etc. 
-!      Removed selection of averaging method. 
-!      SPixel_B and SPixel_Sb re-dimensioned to remove SPixel size. 
-!20th Jan 2012 general tidyup remove data varaible 
+!     4th Dec 2000, Kevin M. Smith: Original version
+!    16th Jan 2001, Kevin M. Smith:
+!       Re-drafted
+!    18th Jan 2001, Kevin M. Smith:
+!       Modified to take array of reflectances etc from SPixel%Surface
+!       Changed name from Get_Surface_Rs to Get_Rs
+!    16th Mar 2001, Andy Smith:
+!       Removed checking for invalid averaging method.
+!       Using named constants for averaging method.
+!    15th Jun 2001, Andy Smith:
+!       Changed error message string assignments/writes.
+!       Long message strings were wrapped over two lines with only one set of
+!       quotes around the whole string (including the line continuation marker).
+!       Original code works on DEC but not Linux.
+!    **************** ECV work starts here *************************************
+!    23rd Feb 2011, Andy Smith:
+!       Cloud flags converted to real to match current ORAC data.
+!    30th Mar 2011, Andy Smith:
+!       Removal of super-pixel averaging. Process single pixels at X0, Y0,
+!       Removed other SPixel indices Xc, Yc, Xn, Yn etc.
+!       Removed selection of averaging method.
+!       SPixel_B and SPixel_Sb re-dimensioned to remove SPixel size.
+!    20th Jan 2012, Somebody:
+!       General tidyup and removed data variable.
+!    30th Jul 2014, Greg McGarragh:
+!       Cleaned up the code.
+!
 ! Bugs:
 !   None known.
 !
 ! $Id$
 !
-!------------------------------------------------------------------------------
+!-------------------------------------------------------------------------------
 subroutine Get_Rs(Ctrl, SPixel, SPixel_b, SPixel_Sb, status)
-   
-    use CTRL_def
-    use Data_def
-    use SPixel_def
 
-    implicit none
+   use CTRL_def
+   use Data_def
+   use SPixel_def
 
-!   Define arguments
+   implicit none
 
-    type(CTRL_t)   :: Ctrl
-     type(SPixel_t) :: SPixel
+   ! Define arguments
 
-    real           :: SPixel_b( Ctrl%Ind%NSolar)
-    real           :: SPixel_Sb(Ctrl%Ind%NSolar, Ctrl%Ind%NSolar)
-    integer :: status
-    
-!   Define local variables
+   type(CTRL_t),   intent(in)    :: Ctrl
+   type(SPixel_t), intent(inout) :: SPixel
+   real,           intent(in)    :: SPixel_b( Ctrl%Ind%NSolar)
+   real,           intent(in)    :: SPixel_Sb(Ctrl%Ind%NSolar, Ctrl%Ind%NSolar)
+   integer,        intent(out)   :: status
 
-    character(180) :: message
+   ! Define local variables
 
-!   Set status to zero
+   character(180) :: message
 
+   ! Set status to zero
    status = 0
 
-    
-!   Check mask to see if pixel is 'good'    
-
+   ! Check mask to see if pixel is 'good'
    if (SPixel%Mask == 1) then
-   
       SPixel%Rs(:)    = SPixel_b( :)
-      SPixel%SRs(:,:) = SPixel_Sb(:, :)	  
-      
-      if (SPixel%Surface%Flags == 1) then 
+      SPixel%SRs(:,:) = SPixel_Sb(:, :)
+
+      if (SPixel%Surface%Flags == 1) then
          SPixel%Surface%Land = 1
          SPixel%Surface%Sea  = 0
       else
          SPixel%Surface%Land = 0
          SPixel%Surface%Sea  = 1
-      end if	  
-               
+      end if
    else
       status = GetRsCentPix
       write(unit=message, fmt=*) 'Get_Rs: pixel contains bad data'
-      call Write_log(Ctrl, trim(message), status) 
+      call Write_log(Ctrl, trim(message), status)
    end if
 
 end subroutine Get_Rs
--- src/GetX.F90
+++ src/GetX.F90
@@ -1,23 +1,23 @@
+!-------------------------------------------------------------------------------
 ! Name:
 !    Get_X
 !
 ! Purpose:
-!    Gets the first guess and a priori state vector values (X0 and Xb) for 
-!    the current super pixel according to the methods specified by SPixel%FG 
-!    and SPixel%AP.
+!    Gets the first guess and a priori state vector values (X0 and Xb) for the
+!    current super pixel according to the methods specified by SPixel%FG and
+!    SPixel%AP.
 !
 ! Arguments:
-!    Name             Type           In/Out   Description
-!    Ctrl             struct         In       Control structure
-!    SAD_CloudClass   struct         In       SAD cloud class structure
-!    SPixel           struct         Both     Super-pixel structure (contains
-!                                             the phase and AP, FG arrays to be
-!                                             set).
-!    status           integer        Out      Error status
-!    
+!    Name           Type    In/Out Description
+!    Ctrl           struct  In     Control structure
+!    SAD_CloudClass struct  In     SAD cloud class structure
+!    SPixel         struct  Both   Super-pixel structure (contains the phase and
+!                                  AP, FG arrays to be set).
+!    status         integer Out    Error status
+!
 ! Algorithm:
-!    Set "first guess" phase to the value for the retrieval (phase is no longer 
-!       retrieved). 
+!    Set "first guess" phase to the value for the retrieval (phase is no longer
+!       retrieved).
 !    Set cloud class depending on the FG phase
 !    For each state variable
 !    - Set a priori state vector and error covariance by the selected method
@@ -27,66 +27,67 @@
 !      limits for the cloud class)
 !    (Note that not all methods are supported for all variables).
 !
-!    Note that when error values are associated with the a priori the values 
-!    must be squared for use in the ECP. It is assumed that the values 
-!    obtained by each method are the rms errors.
+!    Note that when error values are associated with the a priori the values
+!    must be squared for use in the ECP. It is assumed that the values obtained
+!    by each method are the rms errors.
 !
 ! Local variables:
-!    Name        Type      Description
-!    i           int       Loop counter
-!    SetErr      logical   Switch to tell X_MDAD whether to supply
-!                          an error value.
-!    X           real      State variable set by X_MDAD
-!    Err         real      Error in state variable set by X_MDAD
-!    message     char      Log file message
+!    Name Type Description
 !
 ! History:
-!  30th January, 2001, Kevin M. Smith : original version
-!  24th April 2001, Andy Smith:
-!     Using named constants for allowed selection method values.
-!     Moved test for valid selection method to Read_Driver. No need to test
-!     for every super-pixel.
-!     Using SPixel%FGPhase instead of SPixel%X0(6) to store phase.
-!     Added setting of SPixel%FGCloudClass.
-!   8th June 2001, Andy Smith:
-!     Changed structure of first guess phase setting to take account of 
-!     cases when XMDAD setting is requested but unsuccessful.   
-!     Added scaling of Sx.  
-!  11th June 2001, Andy Smith:
-!     Xscale must be squared where used with Sx since Sx contains the square
-!     of the errors in X.
-!   6th July 2001, Andy Smith:
-!     Update to comments.
-!     Added status check on SDAD a priori setting.
-!  17th July 2001, Andy Smith:
-!     Using named constants for Ts errors when set by AUX method. Errors 
-!     now squared.
-!  ***************************** ECV work starts here *********************
-!  21st Mar 2011, Andy Smith:
-!     Removal of functionality to change phase during retrieval. 
-!     Phase will be initialised to the Ctrl value in the main function. 
-!     Since phase is now fixed for the retrieval only 1 cloud class is used, 
-!     hence the first guess cloud class is the 1 available class. 
-!  22nd Mar 2011, Andy Smith:
-!     Removal of phase change, phase 2. Only 1 cloud class rather than an 
-!     array of N cloud classes. Ctrl struct reorganised so that X0, Xb, limits
-!     etc only refer to one fixed phase (N.B. state variable not updated, so 
-!     phase is still "retrieved"). 
-!   5th Apr 2011, Andy Smith:
-!     Removed selection methods SAD and SDAD. Renamed SelmMDAD to SelmMeas.
-!     Mopping up from removal of phase change. Removed setting of Spixel%FGPhase
-!     as it is redundant. 
-!  5th Aug 2011, Caroline Polsen remove rf to sadcloudclass to simplify code
-!     these values can be read from the driver file instead.
-!  20th June 2012 C. Poulsen added in sacura option
-!  2nd October 2012 C. Poulsen set in active statearibaes error to a very low
-!     number i.e assume they are well known and let information csignal go to
-!     active state variables
-! 3rd October 2012 C. Poulsen modified how ctrl%sx is set added skint to be
-!     surface temperature first guess
-! 26th/2/2013 CP added in option to process pixel if only one IR channel is present
-! 15th/1/2014, GM: Fixed setting of SPixel%Sx for inactive state variables so
-!     that Ctrl%Sx is not modified.
+!    30th Jan 2001, Kevin M. Smith: Original version
+!    24th Apr 2001, Andy Smith:
+!       Using named constants for allowed selection method values.
+!       Moved test for valid selection method to Read_Driver. No need to test
+!       for every super-pixel.
+!       Using SPixel%FGPhase instead of SPixel%X0(6) to store phase.
+!       Added setting of SPixel%FGCloudClass.
+!     8th Jun 2001, Andy Smith:
+!       Changed structure of first guess phase setting to take account of cases
+!       when XMDAD setting is requested but unsuccessful.
+!       Added scaling of Sx.
+!    11th Jun 2001, Andy Smith:
+!       Xscale must be squared where used with Sx since Sx contains the square
+!       of the errors in X.
+!     6th Jul 2001, Andy Smith:
+!       Update to comments.
+!       Added status check on SDAD a priori setting.
+!    17th Jul 2001, Andy Smith:
+!       Using named constants for Ts errors when set by AUX method. Errors now
+!       squared.
+!    **************** ECV work starts here *************************************
+!    21st Mar 2011, Andy Smith:
+!       Removal of functionality to change phase during retrieval.
+!       Phase will be initialised to the Ctrl value in the main function.
+!       Since phase is now fixed for the retrieval only 1 cloud class is used,
+!       hence the first guess cloud class is the 1 available class.
+!    22nd Mar 2011, Andy Smith:
+!       Removal of phase change, phase 2. Only 1 cloud class rather than an
+!       array of N cloud classes. Ctrl struct reorganised so that X0, Xb, limits
+!       etc only refer to one fixed phase (N.B. state variable not updated, so
+!       phase is still "retrieved").
+!     5th Apr 2011, Andy Smith:
+!       Removed selection methods SAD and SDAD. Renamed SelmMDAD to SelmMeas.
+!       Mopping up from removal of phase change. Removed setting of
+!       Spixel%FGPhase as it is redundant.
+!     5th Aug 2011, Caroline Polsen:
+!       Remove rf to sadcloudclass to simplify code. These values can be read
+!       from the driver file instead.
+!    20th Jun 2012, Caroline Poulsen:
+!       Added in sacura option
+!     2nd Oct 2012, Caroline Poulsen:
+!       Set in active statearibaes error to a very low number i.e assume they
+!       are well known and let information csignal go to active state variables
+!     3rd Oct 2012, Caroline Poulsen:
+!       Modified how ctrl%sx is set added skint to be surface temperature first
+!       guess
+!    26th Feb 2013, Caroline Poulsen:
+!       Added in option to process pixel if only one IR channel is present
+!    15th Jan 2014, Greg McGarragh:
+!       Fixed setting of SPixel%Sx for inactive state variables so that Ctrl%Sx
+!       is not modified.
+!    30th Jul 2014, Greg McGarragh:
+!       Cleaned up the code.
 !
 ! Bugs:
 !   None known.
@@ -95,165 +96,149 @@
 !
 !------------------------------------------------------------------------------------
 
-Subroutine Get_X(Ctrl, SAD_Chan, SPixel, status)
+subroutine Get_X(Ctrl, SAD_Chan, SPixel, status)
 
-   use ECP_Constants
    use CTRL_def
+   use ECP_Constants
    use SAD_Chan_def
    use SPixel_def
-   
+
    implicit none
-   
-!  Declare arguments
 
-   type(Ctrl_t), intent(inout)           :: Ctrl
-   type(SAD_Chan_t), intent(in)       :: SAD_Chan(Ctrl%Ind%Ny)
-   type(SPixel_t), intent(inout)      :: SPixel
-   integer, intent(out)               :: status   
+   ! Declare arguments
 
-!  Local variables
+   type(Ctrl_t),     intent(inout) :: Ctrl
+   type(SAD_Chan_t), intent(in)    :: SAD_Chan(Ctrl%Ind%Ny)
+   type(SPixel_t),   intent(inout) :: SPixel
+   integer,          intent(out)   :: status
 
-   logical    :: SetErr  ! Tells X_MDAD/X_SDAD that an error value should be set
-   integer    :: i       ! Loop counter
-   real       :: X       ! State variable value returned by X_MDAD
-   real       :: Err     ! Error value returned by X_MDAD
-     
+   ! Local variables
+
+   logical    :: SetErr ! Tells X_MDAD/X_SDAD that an error value should be set
+   integer    :: i      ! Loop counter
+   real       :: X      ! State variable value returned by X_MDAD
+   real       :: Err    ! Error value returned by X_MDAD
+
    SetErr = .false.
-   !SetErr = .true.
-   
-   !  Set a priori and associated error covariance, then set first guess.
-   !  Having set both, check for internal consistency vs the cloud class limits. 
-   !  Loop over all variables, checking the method for each. One loop is used 
-   !  for both AP and FG setting. This is intended to minimise the number of
-   !  operations in incrementing the counter, but the setting of SetErr may offset
-   !  this saving.
 
-   !A priori setting
-   SPixel%Sx = 0 
+   ! Set a priori and associated error covariance, then set first guess.
+   ! Having set both, check for internal consistency vs the cloud class limits.
+   ! Loop over all variables, checking the method for each. One loop is used for
+   ! both AP and FG setting. This is intended to minimise the number of
+   ! operations in incrementing the counter, but the setting of SetErr may
+   ! offset this saving.
+
+   ! Set a priori
+   SPixel%Sx = 0
    do i = 1, MaxStateVar
       if (status /= 0) exit
-      SetErr = .true.   ! Error values are required for a priori
+
+      SetErr = .true. ! Error values are required for a priori
+
       select case (SPixel%AP(i))
 
-      case (SelmMeas)   !  Measurement dependent. Not supported for all variables.
+      case (SelmMeas) ! Measurement dependent. Not supported for all variables.
          call X_MDAD(Ctrl, SAD_Chan, SPixel, i, SetErr, X, Err, status)
          if (status /= XMDADMeth) then
             SPixel%Xb(i)   = X
-            !write(*,*)'GetX:  SPixel%Xb(i) ', SPixel%Xb(i) 
             SPixel%Sx(i,i) = (Err * Ctrl%Invpar%XScale(i)) ** 2
          end if
 
-      case (SelmAUX)    !  AUX method not supported for most vars.
+      case (SelmAUX) ! AUX method not supported for most vars.
          if (i == ITs) then
-            !           Error setting could be much more sophisticated. The current scheme
-            !           takes no account of relative proportions of land/sea in the 
-            !           current SPixel.
-
+            ! Error setting could be much more sophisticated. The current scheme
+            ! takes no account of relative proportions of land/sea in the
+            ! current SPixel.
             SPixel%Xb(i)   = SPixel%RTM%LW%skint
             if (SPixel%Surface%Sea == 1)  &
-                 &  SPixel%Sx(i,i) = (AUXErrTsSea * Ctrl%Invpar%XScale(i)) ** 2
+               SPixel%Sx(i,i) = (AUXErrTsSea * Ctrl%Invpar%XScale(i)) ** 2
             if (SPixel%Surface%Land == 1) &
-                 &  SPixel%Sx(i,i) = (AUXErrTsLand * Ctrl%Invpar%XScale(i)) ** 2	    
+               SPixel%Sx(i,i) = (AUXErrTsLand * Ctrl%Invpar%XScale(i)) ** 2
          end if
-
       end select
-        
-      !     Ctrl method, used if method is Ctrl or other methods failed.
-      !     Ctrl%Sx is squared after reading in.
 
-      if (SPixel%AP(i) == SelmCtrl .or. &   
-           & (SPixel%AP(i) == SelmMeas .and. status == XMDADMeth) ) then  
+      ! Ctrl method, used if method is Ctrl or other methods failed.
+      ! Ctrl%Sx is squared after reading in.
+      if (SPixel%AP(i) == SelmCtrl .or. &
+          (SPixel%AP(i) == SelmMeas .and. status == XMDADMeth) ) then
          SPixel%Xb(i)   = Ctrl%Xb(i)
 
          if (any(SPixel%X .eq. i)) then
             SPixel%Sx(i,i) = (Ctrl%Sx(i) * Ctrl%Invpar%XScale(i)) ** 2
          else
-            ! assume that the inactive state variables are well known do not try to retrieve
+            ! Assume that the inactive state variables are well known do not try
+            ! to retrieve
             SPixel%Sx(i,i) = (1.0e-5     * Ctrl%Invpar%XScale(i)) ** 2
          endif
 
          if (SPixel%Illum(1) == IDay .or. SPixel%Illum(1) == IDaynore) then
-            !check if all IR channels are present 
-            !if only one is present then remove surface temperature state variable from state vector
-            ! by setting uncertainty to a very small number
-            ! SPixel%QC = ibset(SPixel%QC, SPixTemp)
+            ! Check if all IR channels are present
+            ! If only one is present then remove surface temperature state
+            ! variable from state vector by setting uncertainty to a very small
+            ! number SPixel%QC = ibset(SPixel%QC, SPixTemp)
             if (btest(Spixel%QC,SPixTemp )) then
-               ! check at least one thermal channel is present
-               !if ( SPixel%Ym(Ctrl%Ind%ThermalFirst) .or. SPixel%Ym(Ctrl%Ind%ThermalLast) .lt. 2.0) then                  
-               !                  Ctrl%SX(5)=1.0e-5
-               !write(*,*)'ym',SPixel%Ym
-               
-               !pause
-               !               end if
+               ! Check at least one thermal channel is present
             end if
          endif
 
          status = 0
-      end if                                ! End of AP setting.
+      end if
 
+      ! Having set a priori for the variable, set first guess. If the FG method
+      ! is the same as the AP, just copy the AP value. The first "if" is
+      ! slightly dangerous: the assumption is that the first guess method is
+      ! legal and supported. This holds provided that the same methods are
+      ! supported for FG and AP.
 
-      !     Having set a priori for the variable, set first guess. If the FG method is
-      !     the same as the AP, just copy the AP value. The first "if" is slightly 
-      !     dangerous: the assumption is that the first guess method is legal and
-      !     supported. This holds provided that the same methods are supported for
-      !     FG and AP.
-
-      !Set FG
+      ! Set first guess
       if (SPixel%FG(i) == Spixel%AP(i)) then
 
          SPixel%X0(i) = SPixel%Xb(i)
 
-      else 
+      else
          if (status /= 0) exit
-         SetErr = .false.  ! Error values are not required for first guess
-	  
+
+         SetErr = .false. ! Error values are not required for first guess
+
          select case (Spixel%FG(i))
-         case (SelmMeas)   !  MDAD method. Not supported for all variables.
-     
+
+         case (SelmMeas)  ! MDAD method. Not supported for all variables.
             call X_MDAD(Ctrl, SAD_Chan, SPixel, i, SetErr, X, Err, status)
             if( status .ne. 0 ) then
                write(*,*) 'X_MDAD failed with status:',status
                exit
             endif
             SPixel%X0(i) = X
-            !write(*,*)'GetX:  SPixel%X0(i) ',i, SPixel%X0(i) 
-         case (Sacura)   !first guess is set using sacura
-            !	    SPixel%X0(i) =sacuravalue
-         case (SelmAUX)    !  AUX method not supported for most vars.
+         case (Sacura)    ! First guess is set using sacura
+
+         case (SelmAUX)   ! AUX method not supported for most vars.
             if (i == ITs) &
-                 & SPixel%X0(i) = SPixel%RTM%LW%T(SPixel%RTM%LW%Np)
+               SPixel%X0(i) = SPixel%RTM%LW%T(SPixel%RTM%LW%Np)
          end select
 
-!        Ctrl method, used if method is Ctrl or other methods failed.
-         if (SPixel%FG(i) == SelmCtrl .or. &             
-              & (SPixel%FG(i) == SelmMeas .and. status == XMDADMeth)) then   
-            SPixel%X0(i) = Ctrl%X0(i)	 
+         ! Ctrl method, used if method is Ctrl or other methods failed.
+         if (SPixel%FG(i) == SelmCtrl .or. &
+             (SPixel%FG(i) == SelmMeas .and. status == XMDADMeth)) then
+            SPixel%X0(i) = Ctrl%X0(i)
             status = 0
          end if
 
-      end if   ! End of first guess setting    
+      end if
 
-!     Check for internal consistency with the cloud class limits. Set the 
-!     a priori or first guess values equal to any limit they exceed.
-
-      !write(*,*)'SPixel%Xb',SPixel%Xb
-      !write(*,*)'ll',Ctrl%Invpar%xLlim
-      !write(*,*)'il',Ctrl%Invpar%xUlim
+      ! Check for internal consistency with the cloud class limits. Set the a
+      ! priori or first guess values equal to any limit they exceed.
       if (SPixel%Xb(i) > Ctrl%Invpar%xUlim(i)) then
          SPixel%Xb(i) = Ctrl%Invpar%xUlim(i)
       else if (SPixel%Xb(i) < Ctrl%Invpar%xLlim(i)) then
          SPixel%Xb(i) = Ctrl%Invpar%xLlim(i)
       end if
-         
+
       if (SPixel%X0(i) > Ctrl%Invpar%xUlim(i)) then
          SPixel%X0(i) = Ctrl%Invpar%xUlim(i)
       else if (SPixel%X0(i) < Ctrl%Invpar%xLlim(i)) then
          SPixel%X0(i) = Ctrl%Invpar%xLlim(i)
-      end if 
-!write(*,*)'SPixel%Xb i',SPixel%Xb
-!pause        
+      end if
 
+   end do
 
-   end do ! End of state variable loop.
-
-End Subroutine Get_X
+end subroutine Get_X
