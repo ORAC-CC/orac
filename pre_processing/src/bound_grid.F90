15c408cbaa3845b65ff6a05bc4954d0a11fca1b8
Adam Povey <adam.povey@physics.ox.ac.uk>
2014-08-13 14:49:19 +0000

    New, more efficient bilinear interpolation routine for regular input grids.

---
+++ pre_processing/src/bound_grid.F90
@@ -0,0 +1,175 @@
+!-------------------------------------------------------------------------------
+! Name: bound_grid.F90
+!
+! Purpose:
+! For a bilinear interpolation, this selects the four points that surround the
+! desired point and calculates the normalised distance of that from the bottom
+! left corner of the original grid cell (from which the coefficients of the
+! interpolation are calculated).
+!
+! Description and Algorithm details:
+! 1) Determine the index of the grid point just below and left of the desired
+!    point.
+! 2) Unless wrapping is cancelled, if the point is left of the first column or
+!    right of the last column, set these as the corners (wrapping through the
+!    dateline).
+! 3) Set other points outside the grid to be extrapolated.
+! 4) Calculate the normalised distance of the desired point from the bottom
+!    corner (t and u).
+!
+! Arguments:
+! Name     Type    In/Out/Both Description
+! ------------------------------------------------------------------------------
+! xstart   real    In  Initial value of the x axis.
+! nx       integer In  Length of the x axis.
+! x_invdel real    In  1 / the separation of x grid cells.
+! ystart   real    In  Initial value of the y axis.
+! ny       integer In  Length of the y axis.
+! y_invdel real    In  1 / the separation of y grid cells.
+! interp   struct  Out Indices of points just surrounding desired point. (See
+!                      interpol.F90)
+! xout     real    In  Horizontal coordinate at which value desired.
+! yout     real    In  Vertical coordinate at which value desired.
+! no_wrap  logic   Opt If true, the interpolation will not wrap around the
+!                      horizontal axis. Set if the grid to be interpolated is
+!                      not global.
+!
+! History:
+! 2014/08/05, AP: Initial version.
+!
+! $Id$
+!
+! Bugs:
+! None known.
+!-------------------------------------------------------------------------------
+subroutine bound_regular_grid(start, invdel, n, out, below, above, frac, wrap)
+   implicit none
+
+   real(8),     intent(in)  :: start  ! first point on grid
+   real(8),     intent(in)  :: invdel ! 1 / grid division
+   integer,     intent(in)  :: n      ! length of grid
+   real(sreal), intent(in)  :: out    ! desired point
+   integer,     intent(out) :: below  ! grid reference just below out
+   integer,     intent(out) :: above  ! grid reference just above out
+   real(sreal), intent(out) :: frac   ! fraction of cell covered by out
+   logical,     intent(in)  :: wrap
+
+   real(sreal)              :: tmp
+   logical                  :: bottom
+
+   ! index of grid point below desired location
+   tmp = real((real(out, kind=8) - start)*invdel, kind=sreal)
+   below = floor(tmp) + 1
+
+   bottom = .false.
+
+   ! manage locations on the edge of the lon grid
+   if (wrap) then
+      ! loop around the longitude grid
+      if (below < 1) then
+         ! below bottom of grid
+         below = n
+         above = 1
+         bottom = .true.
+      else if (below >= n) then
+         ! above top of grid
+         below = n
+         above = 1
+      else
+         ! within grid
+         above = below + 1
+      end if
+   else
+      ! extrapolate for points beyond the grid
+      if (below < 1) then
+         ! below bottom of grid
+         below = 1
+         above = 2
+      else if (below >= n) then
+         ! above top of grid
+         below = n - 1
+         above = n
+      else
+         ! within grid
+         above = below + 1
+      end if
+   end if
+
+   ! calculate normalised distance from the bottom corner
+   if (bottom) then
+      frac = tmp + 1
+   else
+      frac = tmp + 1 - below
+   end if
+
+end subroutine bound_regular_grid
+
+subroutine bound_irregular_grid(grid, n, out, below, above, frac, wrap)
+   implicit none
+
+   real(sreal), intent(in)  :: grid(:)  ! grid values
+   integer,     intent(in)  :: n        ! length of grid
+   real(sreal), intent(in)  :: out      ! desired point
+   integer,     intent(out) :: below    ! grid reference just below out
+   integer,     intent(out) :: above    ! grid reference just above out
+   real(sreal), intent(out) :: frac     ! fraction of cell covered by out
+   logical,     intent(in)  :: wrap
+
+   integer                  :: mode, old, i
+
+   ! determine if grid increasing or decreasing
+   if (grid(1) < grid(2)) then
+      below = 1
+      above = n
+   else
+      below = n
+      above = 1
+   end if
+
+   mode = 0
+   if (out < grid(below)) then
+      ! value below bottom of grid
+      if (wrap) then
+         below = n
+         above = 1
+         mode = 1
+      else
+         below = 1
+         above = 2
+      end if
+   else if (out > grid(above)) then
+      ! value above top of grid
+      if (wrap) then
+         below = n
+         above = 1
+         mode = 2
+      else
+         below = n-1
+         above = n
+      end if
+   else
+      ! search for coordinates that bound 'out' via bisection
+      old = below   
+      do
+         i = (below + above) / 2
+         if (out > grid(i)) then
+            below = i
+         else if (out < grid(i)) then
+            above = i
+         end if
+         if (i == old) exit
+         old = i
+      end do
+   end if
+
+   ! calculate normalised distance from the bottom corner
+   select case (mode)
+   case(0) ! normal cell
+      frac = (out - grid(below)) / (grid(above) - grid(below))
+   case(1) ! wrapping (in lon) from below bottom cell
+      frac = (out - grid(below) + 360.) / (grid(above) - grid(below) + 360.)
+   case(2) ! wrapping (in lon) from above top cell
+      frac = (out - grid(below)) / (grid(above) - grid(below) + 360.)
+   end select
+
+end subroutine bound_irregular_grid
--- pre_processing/src/interpol.F90
+++ pre_processing/src/interpol.F90
@@ -2,30 +2,280 @@
 ! Name: interpol.F90
 !
 ! Purpose:
-! Container for interpolation routines.
+! Container for interpolation routines. To interpolate a field, call
+! bilinear_coefs and then interp_field. The structure interpol passes information
+! between them.
 !
 ! Description and Algorithm details:
-! None
+! The interpol structure contains the fields:
+!   x0 integer Index of the row just below the desired point.
+!   x1 integer Index of the row just above the desired point.
+!   y0 integer Index of the column just left of the desired point.
+!   y1 integer Index of the column just right of the desired point.
+!   t  real    Fractional distance of desired point from y0.
+!   u  real    Fractional distance of desired point from x0.
 !
 ! Arguments:
 ! None
 !
 ! History:
 ! 2014/05/23, GM: First version.
+! 2014/08/05, AP: New bilinear interpolation routines.
 !
 ! $Id$
 !
 ! Bugs:
-! None known.
+! With arrays, interp_field only checks if the first value is missing.
 !-------------------------------------------------------------------------------
 
 module interpol
 
+   use preproc_constants, only: sreal, real_fill_value
+   
    implicit none
+   
+   type interpol_s
+      integer     :: x0, x1, y0, y1
+      real(sreal) :: t, u
+   end type interpol_s
+
+   interface bilinear_coef
+      module procedure bilinear_coef_reg_reg,  bilinear_coef_reg_irr, &
+                       bilinear_coef_irr_reg,  bilinear_coef_irr_irr, &
+                       bilinear_coef_reg_reg2, bilinear_coef_reg_irr2, &
+                       bilinear_coef_irr_reg2, bilinear_coef_irr_irr2
+   end interface bilinear_coef
+   
+   interface interp_field
+      module procedure interp_field_0d, interp_field_1d, interp_field_2d
+   end interface interp_field
 
 contains
 
+include 'bound_grid.F90'
+   
 include 'interpol_bilinear.F90'
-include 'interpol_nearest_neighbour.F90'
+!include 'interpol_nearest_neighbour.F90'
+
+subroutine bilinear_coef_reg_reg(xstart, x_invdel, nx, ystart, y_invdel, ny, &
+     xout, yout, interp, wrap)
+   implicit none
+
+   real(8),          intent(in)  :: xstart, x_invdel
+   real(8),          intent(in)  :: ystart, y_invdel
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+   logical,          intent(in)  :: wrap
+
+   call bound_regular_grid(xstart, x_invdel, nx, xout, &
+        interp%x0, interp%x1, interp%t, wrap)
+   call bound_regular_grid(ystart, y_invdel, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_reg_reg
+
+subroutine bilinear_coef_reg_irr(xstart, x_invdel, nx, ygrid, ny, &
+     xout, yout, interp, wrap)
+   implicit none
+
+   real(8),          intent(in)  :: xstart, x_invdel
+   real(sreal),      intent(in)  :: ygrid(:)
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+   logical,          intent(in)  :: wrap
+
+   call bound_regular_grid(xstart, x_invdel, nx, xout, &
+        interp%x0, interp%x1, interp%t, wrap)
+   call bound_irregular_grid(ygrid, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_reg_irr
+
+subroutine bilinear_coef_irr_reg(xgrid, nx, ystart, y_invdel, ny, &
+     xout, yout, interp, wrap)
+   implicit none
+
+   real(sreal),      intent(in)  :: xgrid(:)
+   real(8),          intent(in)  :: ystart, y_invdel
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+   logical,          intent(in)  :: wrap
+
+   call bound_irregular_grid(xgrid, nx, xout, &
+        interp%x0, interp%x1, interp%t, wrap)
+   call bound_regular_grid(ystart, y_invdel, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_irr_reg
+
+subroutine bilinear_coef_irr_irr(xgrid, nx, ygrid, ny, &
+     xout, yout, interp, wrap)
+   implicit none
+
+   real(sreal),      intent(in)  :: xgrid(:)
+   real(sreal),      intent(in)  :: ygrid(:)
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+   logical,          intent(in)  :: wrap
+
+   call bound_irregular_grid(xgrid, nx, xout, &
+        interp%x0, interp%x1, interp%t, wrap)
+   call bound_irregular_grid(ygrid, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_irr_irr
+
+subroutine bilinear_coef_reg_reg2(xstart, x_invdel, nx, ystart, y_invdel, ny, &
+     xout, yout, interp)
+   implicit none
+
+   real(8),          intent(in)  :: xstart, x_invdel
+   real(8),          intent(in)  :: ystart, y_invdel
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+
+   call bound_regular_grid(xstart, x_invdel, nx, xout, &
+        interp%x0, interp%x1, interp%t, .true.)
+   call bound_regular_grid(ystart, y_invdel, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_reg_reg2
+
+subroutine bilinear_coef_reg_irr2(xstart, x_invdel, nx, ygrid, ny, &
+     xout, yout, interp)
+   implicit none
+
+   real(8),          intent(in)  :: xstart, x_invdel
+   real(sreal),      intent(in)  :: ygrid(:)
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+
+   call bound_regular_grid(xstart, x_invdel, nx, xout, &
+        interp%x0, interp%x1, interp%t, .true.)
+   call bound_irregular_grid(ygrid, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_reg_irr2
+
+subroutine bilinear_coef_irr_reg2(xgrid, nx, ystart, y_invdel, ny, &
+     xout, yout, interp)
+   implicit none
+
+   real(sreal),      intent(in)  :: xgrid(:)
+   real(8),          intent(in)  :: ystart, y_invdel
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+
+   call bound_irregular_grid(xgrid, nx, xout, &
+        interp%x0, interp%x1, interp%t, .true.)
+   call bound_regular_grid(ystart, y_invdel, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_irr_reg2
+
+subroutine bilinear_coef_irr_irr2(xgrid, nx, ygrid, ny, &
+     xout, yout, interp)
+   implicit none
+
+   real(sreal),      intent(in)  :: xgrid(:)
+   real(sreal),      intent(in)  :: ygrid(:)
+   integer,          intent(in)  :: nx, ny
+   real(sreal),      intent(in)  :: xout, yout
+   type(interpol_s), intent(out) :: interp
+
+   call bound_irregular_grid(xgrid, nx, xout, &
+        interp%x0, interp%x1, interp%t, .true.)
+   call bound_irregular_grid(ygrid, ny, yout, &
+        interp%y0, interp%y1, interp%u, .false.)
+
+end subroutine bilinear_coef_irr_irr2
+
+subroutine interp_field_0d(datin, datout, interp)
+
+   implicit none
+
+   real(sreal), target, intent(in)    :: datin(:,:)
+   real(sreal),         intent(inout) :: datout
+   type(interpol_s),    intent(in)    :: interp
+
+   logical     :: miss(4)
+   real(sreal) :: coef(3)
+   real(sreal) :: bot_left, bot_rght, top_left, top_rght
+
+   ! decide that interpolation to do dependent on the missing values
+   miss = [datin(interp%x0, interp%y0) == real_fill_value, &
+           datin(interp%x1, interp%y0) == real_fill_value, &
+           datin(interp%x0, interp%y1) == real_fill_value, &
+           datin(interp%x1, interp%y1) == real_fill_value]
+
+   bot_left = datin(interp%x0, interp%y0)
+   bot_rght = datin(interp%x1, interp%y0)
+   top_left = datin(interp%x0, interp%y1)
+   top_rght = datin(interp%x1, interp%y1)
+
+   include "interp_field.inc"
+
+end subroutine interp_field_0d
+
+subroutine interp_field_1d(datin, datout, interp)
+
+   implicit none
+
+   real(sreal), target, intent(in)       :: datin(:,:,:)
+   real(sreal),         intent(inout)    :: datout(:)
+   type(interpol_s),    intent(in)       :: interp
+
+   logical                            :: miss(4)
+   real(sreal)                        :: coef(3)
+   real(sreal), pointer, dimension(:) :: bot_left, bot_rght, top_left, top_rght
+
+   ! decide interpolation to do dependent on the missing values
+   miss = [datin(interp%x0, interp%y0, 1) == real_fill_value, &
+           datin(interp%x1, interp%y0, 1) == real_fill_value, &
+           datin(interp%x0, interp%y1, 1) == real_fill_value, &
+           datin(interp%x1, interp%y1, 1) == real_fill_value]
+   
+   bot_left => datin(interp%x0, interp%y0, :)
+   bot_rght => datin(interp%x1, interp%y0, :)
+   top_left => datin(interp%x0, interp%y1, :)
+   top_rght => datin(interp%x1, interp%y1, :)
+
+   include "interp_field.inc"
+
+end subroutine interp_field_1d
+
+subroutine interp_field_2d(datin, datout, interp)
+
+   implicit none
+
+   real(8),     target, intent(in)         :: datin(:,:,:,:)
+   real(8),             intent(inout)      :: datout(:,:)
+   type(interpol_s),    intent(in)         :: interp
+
+   logical                              :: miss(4)
+   real(8)                              :: coef(3)
+   real(8),     pointer, dimension(:,:) :: bot_left, bot_rght, top_left, top_rght
+
+   ! decide interpolation to do dependent on the missing values
+   miss = [datin(interp%x0, interp%y0, 1, 1) == real_fill_value, &
+           datin(interp%x1, interp%y0, 1, 1) == real_fill_value, &
+           datin(interp%x0, interp%y1, 1, 1) == real_fill_value, &
+           datin(interp%x1, interp%y1, 1, 1) == real_fill_value]
+   
+   bot_left => datin(interp%x0, interp%y0, :, :)
+   bot_rght => datin(interp%x1, interp%y0, :, :)
+   top_left => datin(interp%x0, interp%y1, :, :)
+   top_rght => datin(interp%x1, interp%y1, :, :)
+
+   include "interp_field.inc"
+
+end subroutine interp_field_2d
 
 end module interpol
