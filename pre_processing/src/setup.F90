292259ec1765ddba296c609b4f0bf0d6d4ac5b46
Adam Povey <adam.povey@physics.ox.ac.uk>
2014-09-19 17:28:34 +0000

    Bug fix. Reading platform for AVHRR failed to appreciate that there are more letters in METOP than NOAA.

--- pre_processing/src/setup.F90
+++ pre_processing/src/setup.F90
@@ -52,6 +52,8 @@ contains
 ! 2012/11/14, CP: remove platform name from aatsr
 ! 2013/08/14, GT: Added trim to write statement of L1b filename
 ! 2013/09/16, AP: removed channel_flag, preproc_dims, date
+! 2014/09/10, AP: Order of RTTOV channels for AATSR reversed.
+! 2014/09/19, AP: Fixed bug in reading platform for Metop AVHRR.
 !
 ! $Id$
 !
@@ -73,12 +75,14 @@ subroutine setup_modis(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
    character(len=path_length),     intent(in)    :: l1b_path_file
    character(len=path_length),     intent(in)    :: geo_path_file
    character(len=platform_length), intent(out)   :: platform
-   integer(kind=sint),             intent(out)   :: year,month,day,doy,hour,minute
-   character(len=date_length),     intent(out)   :: cyear,cmonth,cday,cdoy,chour,cminute
+   integer(kind=sint),             intent(out)   :: year,month,day,doy
+   integer(kind=sint),             intent(out)   :: hour,minute
+   character(len=date_length),     intent(out)   :: cyear,cmonth,cday
+   character(len=date_length),     intent(out)   :: cdoy,chour,cminute
    type(channel_info_s),           intent(inout) :: channel_info
    logical,                        intent(in)    :: verbose
 
-   integer(kind=sint)                           :: intdummy1,intdummy2
+   integer(kind=sint)                            :: intdummy1,intdummy2
 
    if (verbose) write(*,*) '<<<<<<<<<<<<<<< Entering setup_modis()'
 
@@ -126,26 +130,28 @@ subroutine setup_modis(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
    write(cday, '(i2.2)') day
 
    !now set up the channels
+   channel_info%nviews = 1
+   channel_info%nchannels_total = 6
+   call allocate_channel_info(channel_info)
+
    !numbering wrt instrument definition
-   channel_info%channel_ids_instr= (/ 1, 2, 6, 20, 31, 32 /)
+   channel_info%channel_ids_instr = (/ 1, 2, 6, 20, 31, 32 /)
    !numbering wrt to increasing wl, starting at 1
-   channel_info%channel_ids_abs= (/ 1, 2, 3, 4, 5, 6 /)
+   channel_info%channel_ids_abs = (/ 1, 2, 3, 4, 5, 6 /)
+   !which are the wl of those channels (approximate)
+   channel_info%channel_wl_abs = (/ 0.67, 0.87, 1.60, 3.70, 11.017, 12.032 /)
+   channel_info%channel_view_ids = 1
 
    !which channels have sw/lw components
-   channel_info%channel_sw_flag=(/ 1, 1, 1, 1, 0, 0 /)
-   channel_info%channel_lw_flag=(/ 0, 0, 0, 1, 1, 1 /)
-   channel_info%nchannels_sw=sum(channel_info%channel_sw_flag)
-   allocate(channel_info%channel_ids_rttov_coef_sw(channel_info%nchannels_sw))
-   channel_info%nchannels_lw=sum(channel_info%channel_lw_flag)
-   allocate(channel_info%channel_ids_rttov_coef_lw(channel_info%nchannels_lw))
-
-   !process everything
-   channel_info%channel_proc_flag=(/ 1, 1, 1, 1, 1, 1 /)
-   !which are the wl of those channels (approximate)
-   channel_info%channel_wl_abs=(/ 0.67, 0.87, 1.60, 3.70, 11.017, 12.032 /)
+   channel_info%channel_sw_flag = (/ 1, 1, 1, 1, 0, 0 /)
+   channel_info%nchannels_sw = 4
+   channel_info%channel_lw_flag = (/ 0, 0, 0, 1, 1, 1 /)
+   channel_info%nchannels_lw = 3
    !channel number wrt RTTOV coefficient file
-   channel_info%channel_ids_rttov_coef_sw=(/ 1, 2, 6, 20 /)
-   channel_info%channel_ids_rttov_coef_lw=(/ 1, 11, 12 /)
+   allocate(channel_info%channel_ids_rttov_coef_sw(channel_info%nchannels_sw))
+   channel_info%channel_ids_rttov_coef_sw = (/ 1, 2, 6, 20 /)
+   allocate(channel_info%channel_ids_rttov_coef_lw(channel_info%nchannels_lw))
+   channel_info%channel_ids_rttov_coef_lw = (/ 1, 11, 12 /)
 
    if (verbose) write(*,*) '>>>>>>>>>>>>>>> Leaving setup_modis()'
 
@@ -165,12 +171,14 @@ subroutine setup_avhrr(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
    character(len=path_length),     intent(in)    :: l1b_path_file
    character(len=path_length),     intent(in)    :: geo_path_file
    character(len=platform_length), intent(out)   :: platform
-   integer(kind=sint),             intent(out)   :: year,month,day,doy,hour,minute
-   character(len=date_length),     intent(out)   :: cyear,cmonth,cday,cdoy,chour,cminute
+   integer(kind=sint),             intent(out)   :: year,month,day,doy
+   integer(kind=sint),             intent(out)   :: hour,minute
+   character(len=date_length),     intent(out)   :: cyear,cmonth,cday
+   character(len=date_length),     intent(out)   :: cdoy,chour,cminute
    type(channel_info_s),           intent(inout) :: channel_info
    logical,                        intent(in)    :: verbose
 
-   integer(kind=sint)                           :: intdummy1,intdummy2
+   integer(kind=sint)                            :: intdummy1,intdummy2
 
    if (verbose) write(*,*) '<<<<<<<<<<<<<<< Entering setup_avhrr()'
 
@@ -193,7 +201,8 @@ subroutine setup_avhrr(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
 
    !which avhrr are we processing?
    intdummy1=index(trim(adjustl(l1b_path_file)),'.h5',back=.true.)
-   platform=trim(adjustl(l1b_path_file(intdummy1-52:intdummy1-47)))
+   intdummy2=index(trim(adjustl(l1b_path_file)),'/',back=.true.)
+   platform=trim(adjustl(l1b_path_file(intdummy2+1:intdummy1-47)))
 
    !Get year, month,day,hour and minute as character
    cyear=trim(adjustl(l1b_path_file(intdummy1-45:intdummy1-42)))
@@ -213,27 +222,31 @@ subroutine setup_avhrr(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
    call GREG2DOY(year,month,day,doy)
    write(cdoy, '(i3.3)') doy
 
-   !now set up the channels (AP: Strictly, this definition does nothing.)
+   !now set up the channels
+   channel_info%nviews = 1
+   channel_info%nchannels_total = 6
+   call allocate_channel_info(channel_info)
+
    !numbering wrt instrument definition
    !3=3B,6=3A (that's how it is in the HDF5 input file)
-   channel_info%channel_ids_instr= (/ 1, 2, 3, 4, 5, 6 /)
+   channel_info%channel_ids_instr = (/ 1, 2, 3, 4, 5, 6 /)
    !numbering wrt to increasing wl, starting at 1
    !3=3A,4=3B
-   channel_info%channel_ids_abs= (/ 1, 2, 3, 4, 5, 6 /)
-   !which channels have sw/lw components
-   channel_info%channel_sw_flag=(/ 1, 1, 1, 1, 0, 0 /)
-   channel_info%channel_lw_flag=(/ 0, 0, 0, 1, 1, 1 /)
-   channel_info%nchannels_sw=sum(channel_info%channel_sw_flag)
-   allocate(channel_info%channel_ids_rttov_coef_sw(channel_info%nchannels_sw))
-   channel_info%nchannels_lw=sum(channel_info%channel_lw_flag)
-   allocate(channel_info%channel_ids_rttov_coef_lw(channel_info%nchannels_lw))
-   !process everything
-   channel_info%channel_proc_flag=(/ 1, 1, 1, 1, 1, 1 /)
+   channel_info%channel_ids_abs = (/ 1, 2, 3, 4, 5, 6 /)
    !which are the wl of those channels (approximate)
-   channel_info%channel_wl_abs=(/ 0.63, 0.8625, 1.61, 3.74, 10.8, 12.0 /)
+   channel_info%channel_wl_abs = (/ 0.63, 0.8625, 1.61, 3.74, 10.8, 12.0 /)
+   channel_info%channel_view_ids = 1
+
+   !which channels have sw/lw components
+   channel_info%channel_sw_flag = (/ 1, 1, 1, 1, 0, 0 /)
+   channel_info%nchannels_sw = 4
+   channel_info%channel_lw_flag = (/ 0, 0, 0, 1, 1, 1 /)
+   channel_info%nchannels_lw = 3
    !channel number wrt RTTOV coefficient file
-   channel_info%channel_ids_rttov_coef_sw=(/ 1, 2, 3, 4 /)
-   channel_info%channel_ids_rttov_coef_lw=(/ 1, 2, 3 /)
+   allocate(channel_info%channel_ids_rttov_coef_sw(channel_info%nchannels_sw))
+   channel_info%channel_ids_rttov_coef_sw = (/ 1, 2, 3, 4 /)
+   allocate(channel_info%channel_ids_rttov_coef_lw(channel_info%nchannels_lw))
+   channel_info%channel_ids_rttov_coef_lw = (/ 1, 2, 3 /)
 
    if (verbose) write(*,*) '>>>>>>>>>>>>>>> Leaving setup_avhrr()'
 
@@ -255,12 +268,14 @@ subroutine setup_aatsr(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
    character(len=path_length),     intent(in)    :: l1b_path_file
    character(len=path_length),     intent(in)    :: geo_path_file
    character(len=platform_length), intent(out)   :: platform
-   integer(kind=sint),             intent(out)    :: year,month,day,doy,hour,minute
-   character(len=date_length),     intent(out)   :: cyear,cmonth,cday,cdoy,chour,cminute
+   integer(kind=sint),             intent(out)   :: year,month,day,doy
+   integer(kind=sint),             intent(out)   :: hour,minute
+   character(len=date_length),     intent(out)   :: cyear,cmonth,cday
+   character(len=date_length),     intent(out)   :: cdoy,chour,cminute
    type(channel_info_s),           intent(inout) :: channel_info
    logical,                        intent(in)    :: verbose
 
-   integer(kind=sint)                           :: intdummy1
+   integer(kind=sint)                            :: intdummy1
 
    if (verbose) write(*,*) '<<<<<<<<<<<<<<< Entering setup_aatsr()'
 
@@ -301,31 +316,29 @@ subroutine setup_aatsr(l1b_path_file,geo_path_file,platform,year,month,day,doy, 
    write(cdoy, '(i3.3)') doy
 
    ! now set up the channels
-   ! Number of viewing geometries: AATSR of course has two, but initially
-   ! we'll pretend it only has one.
    channel_info%nviews = 1
+   channel_info%nchannels_total = 6
+   call allocate_channel_info(channel_info)
+
    ! numbering wrt instrument definition
    ! AATSR has a 0.55 micron channel so instrument channel numbering starts at 2
-   channel_info%channel_ids_instr= (/ 2, 3, 4, 5, 6, 7 /)
+   channel_info%channel_ids_instr = (/ 2, 3, 4, 5, 6, 7 /)
    ! numbering wrt to increasing wl, starting at 1 (0.67 micron)
-   channel_info%channel_ids_abs= (/ 1, 2, 3, 4, 5, 6 /)
-   ! which channels have sw/lw components
-   channel_info%channel_sw_flag=(/ 1, 1, 1, 1, 0, 0 /)
-   channel_info%channel_lw_flag=(/ 0, 0, 0, 1, 1, 1 /)
-   channel_info%nchannels_sw=sum(channel_info%channel_sw_flag)
-   allocate(channel_info%channel_ids_rttov_coef_sw(channel_info%nchannels_sw))
-   channel_info%nchannels_lw=sum(channel_info%channel_lw_flag)
-   allocate(channel_info%channel_ids_rttov_coef_lw(channel_info%nchannels_lw))
-
-   ! View index of each channel - trivial as (for now) dealing with one only
-   channel_info%channel_view_ids=(/ 1, 1, 1, 1, 1, 1 /)
-   !process everything
-   channel_info%channel_proc_flag=(/ 1, 1, 1, 1, 1, 1 /)
+   channel_info%channel_ids_abs = (/ 1, 2, 3, 4, 5, 6 /)
    !which are the wl of those channels (approximate)
-   channel_info%channel_wl_abs=(/ 0.67, 0.87, 1.61, 3.74, 10.8, 12.0 /)
-   !channel number wrt RTTOV coefficient file
-   channel_info%channel_ids_rttov_coef_sw=(/ 2, 3, 4, 5 /)
-   channel_info%channel_ids_rttov_coef_lw=(/ 1, 2, 3 /)
+   channel_info%channel_wl_abs = (/ 0.67, 0.87, 1.61, 3.74, 10.8, 12.0 /)
+   channel_info%channel_view_ids = 1
+
+   ! which channels have sw/lw components
+   channel_info%channel_sw_flag = (/ 1, 1, 1, 1, 0, 0 /)
+   channel_info%nchannels_sw = 4
+   channel_info%channel_lw_flag = (/ 0, 0, 0, 1, 1, 1 /)
+   channel_info%nchannels_lw = 3
+   !channel number wrt RTTOV coefficient file (channels in ascending wavenumber)
+   allocate(channel_info%channel_ids_rttov_coef_sw(channel_info%nchannels_sw))
+   channel_info%channel_ids_rttov_coef_sw = (/ 5, 4, 3, 2 /)
+   allocate(channel_info%channel_ids_rttov_coef_lw(channel_info%nchannels_lw))
+   channel_info%channel_ids_rttov_coef_lw = (/ 3, 2, 1 /)
 
    if (verbose) write(*,*) '>>>>>>>>>>>>>>> Leaving setup_aatsr()'
 
