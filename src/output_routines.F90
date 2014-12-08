a0bae0577bafb8ce7444e0cd92e797e966568d52
Caroline Poulsen <caroline.poulsen@stfc.ac.uk>
2014-12-08 12:15:22 +0000

    added in cloud albedo

--- src/output_routines.F90
+++ src/output_routines.F90
@@ -20,10 +20,10 @@
 !    13th Jun 2014, Greg McGarragh : Original version, output_data_*
 !       structures taken from SPixel module.
 !    16th Sep 2014, Greg McGarragh : Added output_utils.F90.
-!    24th Oct 2014, Oliver Sus: added variables cldtype, cloudmask, cccot_pre, 
-!       lusflags, dem, and nisemask
-!    25th Nov 2014, Adam Povey: Move scaling/offset definitions here.
-!     1st Dec 2014, Oliver Sus: new cldtype_vmax = 9
+!    24th Oct 2014, Oliver Sus: added variables cldtype, cloudmask, cccot_pre, lusflags,
+!       dem, and nisemask
+!    01/12/2014 CP: added cloud albedo
+
 !
 ! Bugs:
 !    None known.
@@ -54,7 +54,14 @@ module output_routines
       integer                       :: vid_convergence
       integer                       :: vid_niter
       integer                       :: vid_phase
-      integer                       :: vid_costja, vid_costjm
+
+	integer,dimension(:), pointer :: vid_cloud_albedo
+
+    
+
+      integer                       :: vid_costja
+      integer                       :: vid_costjm
+
       integer                       :: vid_lsflag
       integer                       :: vid_qcflag
       integer                       :: vid_illum
@@ -235,10 +242,13 @@ module output_routines
       integer(kind=sint)            :: dem_vmin   = 0
       integer(kind=sint)            :: dem_vmax   = 10000
 
-      integer(kind=byte)            :: nisemask_scale  = 1
-      integer(kind=byte)            :: nisemask_offset = 0
-      integer(kind=byte)            :: nisemask_vmin   = 0
-      integer(kind=byte)            :: nisemask_vmax   = 1
+
+      real(kind=sreal),   dimension(:), pointer :: cloud_albedo_scale,cloud_albedo_offset
+      integer(kind=sint), dimension(:), pointer :: cloud_albedo_vmin,cloud_albedo_vmax
+
+
+      integer(kind=byte)            :: nisemask_scale,nisemask_offset
+      integer(kind=byte)            :: nisemask_vmin,nisemask_vmax
 
       real(kind=dreal),   dimension(:,:),   pointer :: time
 
@@ -263,6 +273,10 @@ module output_routines
 
 !     integer(kind=sint), dimension(:,:),   pointer :: albedo
 !     integer(kind=sint), dimension(:,:),   pointer :: albedo_error
+
+
+     integer(kind=sint), dimension(:,:,:),   pointer :: cloud_albedo
+
 
       integer(kind=sint), dimension(:,:),   pointer :: stemp
       integer(kind=sint), dimension(:,:),   pointer :: stemp_error
