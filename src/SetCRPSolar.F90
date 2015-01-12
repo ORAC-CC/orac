912adb40d5d158af8fd75e2fa8b61b9f2a629cda
Caroline Poulsen <caroline.poulsen@stfc.ac.uk>
2015-01-12 14:19:42 +0000

    bug fix to cloud albedo added irfbd/rfbd variable for cloud albedo retrievals

--- src/SetCRPSolar.F90
+++ src/SetCRPSolar.F90
@@ -143,6 +143,27 @@ subroutine Set_CRP_Solar(Ctrl, Ind, GZero, SAD_LUT, CRPOut, dCRPOut, status)
 
    ! Call functions to interpolate the arrays: TFd and RFD are interpolated only
    ! in Tau and Re.
+
+
+
+
+
+   !
+   !now add in an interpolation of RD to solar zenith  which is equivalent to black sky cloud albedo
+   !
+   !only do if solar zenith angle lt. 90 because LUT tables doe not extend very far
+
+
+   call Int_LUT_TauSolRe(SAD_LUT%rfbd(Ind%SolarFirst:Ind%SolarLast,:,:,:), &
+           SAD_LUT%Grid, GZero, Ctrl, CRPOut(:,Irfbd), dCRPOut(:,Irfbd,:), &
+           Irfbd, status)
+
+
+   call Int_LUT_TauSolRe(SAD_LUT%TFbd(Ind%SolarFirst:Ind%SolarLast,:,:,:), &
+           SAD_LUT%Grid, GZero, Ctrl, CRPOut(:,ITFbd), dCRPOut(:,ITFBd,:), &
+           ITFBd, status)
+
+
    call Int_LUT_TauRe(SAD_LUT%RFd(Ind%SolarFirst:Ind%SolarLast,:,:), &
            SAD_LUT%Grid, GZero, Ctrl, CRPOut(:,IRFd), dCRPOut(:,IRFd,:), &
            IRFd, status)
@@ -160,15 +181,7 @@ subroutine Set_CRP_Solar(Ctrl, Ind, GZero, SAD_LUT, CRPOut, dCRPOut, status)
            SAD_LUT%Grid, GZero, Ctrl, CRPOut(:,ITB_u), dCRPOut(:,ITB_u,:), &
            ITB, 0, 0, status)
 
-   call Int_LUT_TauSolRe(SAD_LUT%TFbd(Ind%SolarFirst:Ind%SolarLast,:,:,:), &
-           SAD_LUT%Grid, GZero, Ctrl, CRPOut(:,ITFbd), dCRPOut(:,ITFBd,:), &
-           ITFBd, status)
 
-   ! interpolation of RD to solar zenith which is equivalent to black sky cloud
-   ! albedo
-   call Int_LUT_TauSatRe(SAD_LUT%Rd(Ind%SolarFirst:Ind%SolarLast,:,:,:), &
-           SAD_LUT%Grid, GZero, Ctrl, CRPOut(:,IRd), dCRPOut(:,IRd,:), &
-           IRd, 0, 0, status)
 
    ! Td is interpolated in Tau, SatZen and Re Only process the channels that are
    ! exclusively solar. Channels with a thermal component are interpolated by
@@ -188,6 +201,8 @@ subroutine Set_CRP_Solar(Ctrl, Ind, GZero, SAD_LUT, CRPOut, dCRPOut, status)
    call Int_LUT_TauSatSolAziRe(SAD_LUT%RBd(Ind%SolarFirst:Ind%SolarLast,:,:,:,:,:), &
            SAD_LUT%Grid, GZero, Ctrl, CRPOut(:, IRBd), dCRPOut(:,IRBd,:), &
            iRBd, status)
+
+
 
 #ifdef DEBUG
 !   write(*,*) ' SetCRPSolar: Tb values (2 channels only)'
@@ -241,5 +256,6 @@ subroutine Set_CRP_Solar(Ctrl, Ind, GZero, SAD_LUT, CRPOut, dCRPOut, status)
       write(*,*)
    end do
 #endif
+
 
 end subroutine Set_CRP_Solar
