264d2d9db2d8d6568fb0a6de0cf812c3fef41553
Oliver Sus <oliver.sus@dwd.de>
2015-04-13 13:33:18 +0000

    Reverting previous commit and going back to revision 2991

--- pre_processing/setup.F90
+++ pre_processing/setup.F90
@@ -66,8 +66,6 @@ contains
 !    run time.
 ! 2015/03/10, GM: Changes to support AATSR channel 1 and MODIS channels 3, 4, 5,
 !    and 7 for sea surface reflectance.
-! 2015/04/11, GM: Fixed a bug in the setting of the LW RTTOV channels
-!    for SEVIRI.
 !
 ! $Id$
 !
@@ -590,10 +588,10 @@ subroutine setup_seviri(l1b_path_file,geo_path_file,platform,year,month,day, &
       (/ 0,     0,    0,    1,    1,    1,    1,    1,    1,     1,     1 /)
 
    integer, parameter :: all_channel_ids_rttov_coef_sw(all_nchannels_total) = &
-      (/ 1,     2,    3,    4,    0,    0,    0,    0,    0,     0,     0 /)
+      (/ 1,     2,    3,    4,    5,    6,    7,    8,    9,     10,    11 /)
 
    integer, parameter :: all_channel_ids_rttov_coef_lw(all_nchannels_total) = &
-      (/ 0,     0,    0,    1,    2,    3,    4,    5,    6,     7,     8 /)
+      (/ 0,     0,    0,    0,    0,    0,    0,    0,    0,     0,     0 /)
 
    integer, parameter :: all_map_ids_abs_to_ref_band_land(all_nchannels_total) = &
       (/ 1,     2,    6,    0,    0,    0,    0,    0,    0,     0,     0 /)
