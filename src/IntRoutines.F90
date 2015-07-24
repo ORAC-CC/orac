a7f6e8afc90a207721594697e925f6c2cf4229f9
Greg McGarragh <gregory.mcgarragh@physics.ox.ac.uk>
2015-07-24 19:40:47 +0000

    More C #include single quotes.

--- src/IntRoutines.F90
+++ src/IntRoutines.F90
@@ -27,13 +27,13 @@ module Int_Routines_def
    end interface find_in_array
 contains
 
-#include 'Bcuint.F90'
-#include 'Bcucof.F90'
-#include 'Linint.F90'
-#include 'Locate.F90'
-#include 'Spline.F90'
-#include 'Tridag.F90'
-#include 'polint.F90'
+#include "Bcuint.F90"
+#include "Bcucof.F90"
+#include "Linint.F90"
+#include "Locate.F90"
+#include "Spline.F90"
+#include "Tridag.F90"
+#include "polint.F90"
 
 
 !---------------------------------------------------------------------
