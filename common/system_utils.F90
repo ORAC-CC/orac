6354e0fc70f04ac32a76788751af7c511e79b5e9
Greg McGarragh <gregory.mcgarragh@physics.ox.ac.uk>
2015-10-07 02:17:11 +0000

    Make use of subroutine match_file() in the common/system_utils module to search for MODIS reflectance files.  No more need to hard-wire processing dates.

---
+++ common/system_utils.F90
@@ -0,0 +1,77 @@
+!-------------------------------------------------------------------------------
+! Name: system_utils.F90
+!
+! Purpose:
+!
+! History:
+! 2015/10/05, GM: Original version.
+!
+! $Id$
+!-------------------------------------------------------------------------------
+
+module system_utils
+
+   implicit none
+
+   private
+
+   public :: match_file
+
+contains
+
+!-------------------------------------------------------------------------------
+! Name: match_file
+!
+! Purpose:
+!
+! Description and Algorithm details:
+! None
+!
+! Arguments:
+! Name Type In/Out/Both Description
+!
+! History:
+! 2015/10/05, GM: Original version.
+!
+! Bugs:
+! None known.
+!-------------------------------------------------------------------------------
+function match_file(dir_name, file_pattern, file_name) result(status)
+
+   use iso_c_binding
+
+   implicit none
+
+   character(len=*), intent(in)  :: dir_name
+   character(len=*), intent(in)  :: file_pattern
+   character(len=*), intent(out) :: file_name
+   integer                       :: status
+
+   interface
+      integer(c_int) function system_utils_match_file(dir_name, file_pattern, &
+         file_name, n) bind(C,name='system_utils_match_file')
+         use iso_c_binding
+
+         implicit none
+
+         character(c_char), dimension(*) :: dir_name
+         character(c_char), dimension(*) :: file_pattern
+         character(c_char), dimension(*) :: file_name
+         integer(c_int), value           :: n
+      end function system_utils_match_file
+   end interface
+
+   status = 0
+
+   if (system_utils_match_file(trim(dir_name)//C_NULL_CHAR, &
+                                  trim(file_pattern)//C_NULL_CHAR, &
+                                  file_name, len(file_name)) .le. 0) then
+      status = 1
+      return
+   end if
+
+   file_name = file_name(1:index(file_name, C_NULL_CHAR))
+
+end function match_file
+
+end module system_utils
---
+++ common/system_utils_c.c
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Name: system_utils_c.c
+ *
+ * Purpose:
+ *
+ * History:
+ * 2015/10/05, GM: Original version.
+ *
+ * $Id$
+ ******************************************************************************/
+
+#include <dirent.h>
+#include <regex.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/*******************************************************************************
+ * Name: system_utils_match_file
+ *
+ * Purpose:
+ *
+ * Description and Algorithm details:
+ *
+ * Arguments:
+ * Name : Description
+ *
+ * History:
+ * 2015/10/05, GM: Original version.
+ *
+ * $Id$
+ *
+ * Bugs:
+ * None known.
+ ******************************************************************************/
+int system_utils_match_file(const char *dir_name, const char *file_regex,
+                            char *file_name, int n)
+{
+    int flag;
+
+    DIR           *dir;
+    struct dirent *entry;
+
+    regex_t regex;
+
+    if ((dir = opendir(dir_name)) == NULL) {
+        fprintf(stderr, "ERROR: Error opening directory: %s\n", dir_name);
+        return -1;
+    }
+
+    if (regcomp(&regex, file_regex, 0)) {
+        fprintf(stderr, "ERROR: Error compiling regular expression: %s\n", file_regex);
+        return -1;
+    }
+
+    flag = 0;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (regexec(&regex, entry->d_name, 0, NULL, 0) == 0) {
+            if (file_name)
+                strncpy(file_name, entry->d_name, n);
+            flag = 1;
+            break;
+        }
+    }
+
+    closedir(dir);
+
+    return flag;
+}
