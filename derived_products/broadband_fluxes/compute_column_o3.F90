167cdbed934108184379ed3eb64e0da369e773e6
Matthew Christensen <matthew.christensen@physics.ox.ac.uk>
2016-02-19 11:23:05 +0000

    Compute column ozone (colO3) from input profile of geopoential height and ozone. The computation is a simple summation of integrated ozone over each layer in the atmosphere.

---
+++ derived_products/broadband_fluxes/compute_column_o3.F90
@@ -0,0 +1,50 @@
+!-------------------------------------------------------------------------------
+! Name: compute_column_o3.F90
+!
+! Purpose:
+! Compute column ozone (colO3) from input profile of geopoential height and
+! ozone. The computation is a simple summation of integrated ozone over each
+! layer in the atmosphere.
+!
+! Inputs:
+! geopotential height (km)
+! Ozone (kg/kg)
+!
+! Output:
+! column ozone (DU)
+!
+! History:
+! 2016/02/18, MC: Implementation
+!
+! $Id$
+!
+! Bugs:
+! none.
+!
+!-------------------------------------------------------------------------------
+subroutine compute_column_o3(nlm,H,O3,colO3)
+
+   implicit none
+   
+   integer, intent(in) :: nlm
+
+   !meteorological profiles
+   real, intent(in), dimension(nlm+1) :: &
+    H   ,& !height profile at SAT. pixel               (hPa).
+    O3     !temperature profile at SAT. pixel        (kg/kg).  
+
+   !OUTPUT
+   real, intent(out) :: colO3
+
+   !local
+   integer :: i
+
+   colO3 = 0.
+   !integrate over each level
+   do i=1,nlm-1
+      !print*,H(i),H(i)-H(i+1)
+    colO3 = colO3+((O3(i)+O3(i+1))/2.) * (H(i)-H(i+1))*1000000.
+    !print*,((O3(i)+O3(i+1))/2.) * (H(i)-H(i+1)),colO3
+   enddo
+   return
+end subroutine compute_column_o3
