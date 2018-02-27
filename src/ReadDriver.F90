3206db75dcaaab949ecef47cb7b3c48b426317dd
Martin Stengel <martin.stengel@dwd.de>
2018-02-27 14:19:35 +0000

    Setting lower limit of Tskin to 200K

--- src/ReadDriver.F90
+++ src/ReadDriver.F90
@@ -141,6 +141,9 @@
 ! 2017/10/04, GM: Add the option to use ANN phase to select pixels to process.
 ! 2017/12/13, GT: Changed SLSTR instrument name string to SLSTR-Sentinel3a
 !                 (in preparation for Sentinel-3b)
+! 2017/12/14, MST: the lower limit of tskin was set to 250.K which was too high
+!     in particular for Greenland and Antartica. Have set it to 200.0K.
+!     Comparisons of lw fluxes to CERES confirm that this was necessary.
 !
 ! $Id$
 !
@@ -562,7 +565,7 @@ subroutine Read_Driver(Ctrl, global_atts, source_atts)
    Ctrl%Invpar%XLLim(IRe2)            = switch_cls(c2,Default=0.1)
    Ctrl%Invpar%XLLim(IPc2)            = switch_cls(c2,Default=10.0)
    Ctrl%Invpar%XLLim(IFr2)            = switch_cls(c2,Default=0.0)
-   Ctrl%Invpar%XLLim(ITs)             = switch_app(a, Default=250.0)
+   Ctrl%Invpar%XLLim(ITs)             = switch_app(a, Default=200.) 
    Ctrl%Invpar%XLLim(IRs(:,IRho_0V))  = switch_app(a, Default=0.00001, Cld=0.)
    Ctrl%Invpar%XLLim(IRs(:,IRho_0D))  = switch_app(a, Default=0.00001, Cld=0.)
    Ctrl%Invpar%XLLim(IRs(:,IRho_DV))  = switch_app(a, Default=0.00001, Cld=0.)
