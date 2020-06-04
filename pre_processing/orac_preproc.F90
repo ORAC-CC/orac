ef4c8dda2c5d74bb56187efdc841d60bdceff74d
Gareth Thomas <garethethomas@users.noreply.github.com>
2020-06-04 14:57:49 +0100

    Added in code to allow (A)ATSR orbits to be broken into three segments (night at start of orbit and at end of orbit, and day). This done using the day_night argument to read_aatsr_dimension().

--- pre_processing/orac_preproc.F90
+++ pre_processing/orac_preproc.F90
@@ -309,6 +309,10 @@
 !                 instead of IMPF (as previous). The new driver file option
 !                 USE_GSICS enables this to be disabled.
 ! 2018/10/08, SP: Add support for the GOES-Imager series of sensors (G12-15)
+! 2019/05/21, GT: Added code to run ECMWF data reading and RTTOV running
+!                 separately for East and West portions of an image that
+!                 crosses the date line, rather than populating the entire
+!                 lat-lon range.
 ! 2019/8/14, SP: Add Fengyun-4A support.
 !
 ! Bugs:
@@ -485,6 +489,9 @@ subroutine orac_preproc(mytask, ntasks, lower_bound, upper_bound, driver_path_fi
    ! get number of arguments
    nargs = command_argument_count()
 #endif
+
+   ! Minimal output to notify start and end of programme
+   write(*,*) 'Beginning orac_preproc'
 
    ! Set defaults for optional arguments/fields
    nullify(preproc_opts%channel_ids)
@@ -1404,6 +1411,10 @@ subroutine orac_preproc(mytask, ntasks, lower_bound, upper_bound, driver_path_fi
 
    ! deallocate optional arguments
    if (associated(preproc_opts%channel_ids)) deallocate(preproc_opts%channel_ids)
+
+   ! Notify of sucessful completion
+   write(*,*) 'orac_preproc is complete. Output written to:'
+   Write(*,*) trim(output_path)
 
 #ifdef WRAPPER
 end subroutine orac_preproc
