ddb5bb6b7e5e858633d9ec3eb8412a8ca1cbe92d
simonrp84 <simon.proud@physics.ox.ac.uk>
2020-07-31 09:35:49 +0100

    Tidy up SLSTR reader tool, set more options for showing/saving, remove unnecessary loads.

--- tools/slstr_ml/satpy_read_SLSTR.py
+++ tools/slstr_ml/satpy_read_SLSTR.py
@@ -1,119 +1,130 @@
-#This routine plots out some useful images to help undertand and diagnose your results
+"""Plot some useful images to help undertand and diagnose your results."""
 
-import matplotlib.pyplot as plt
-import numpy as np
-import satpy
-import Visualisation as vis
-from satpy import Scene
-import glob
-from matplotlib import colors as mcolors
 
 def norm(band):
-    """ Normalises the bands for the false color image"""
+    """ Normalises the bands for the false color image."""
     band_min, band_max = band.min(), band.max()
     return ((band - band_min) / (band_max - band_min))
 
-filenames = glob.glob('/g/data/k10/cp2786/fouthyear/S3A_SL_1_RBT____20170418T101506_20170418T101806_20181003T020407_0180_016_336______LR1_R_NT_003.SEN3/*')
 
-def plot_slstr(filenames,mask):
+def plot_slstr(filenames,
+               mask=None,
+               verbose=False,
+               save=True,
+               show=False,
+               odir=None):
+    """Plot the SLSTR granule."""
+    from matplotlib import colors as mcolors
+    import matplotlib.pyplot as plt
+    # import Visualisation as vis
+    from satpy import Scene
+    import numpy as np
+
+    if save and odir is None:
+        print("ERROR: You must specify an output directory for saving.")
+        quit()
 
     scn = Scene(filenames=filenames, reader='slstr_l1b')
     scn.load(['S1_an', 'S2_an', 'S3_an', 'S4_an', 'S5_an', 'S6_an', 'S7_in',
               'S8_in', 'S9_in', 'bayes_an', 'bayes_in', 'cloud_an',
               'latitude_an', 'longitude_an',
               'satellite_zenith_angle_n',
-              'solar_zenith_angle_n','confidence_an',
-    ])
-    print('scn info',scn)
+              'solar_zenith_angle_n', 'confidence_an'])
+    if verbose:
+        print('scn info', scn)
 
-    #plot out the channels
-    plt.imshow(scn['S1_an'], cmap='gray',vmin=0,vmax=100)
+    # Plot the channels
+    plt.imshow(scn['S1_an'], cmap='gray', vmin=0, vmax=100)
     plt.colorbar()
     plt.title('S1')
-    plt.show()
-    
-    plt.imshow(scn['S2_an'], cmap='jet',vmin=0,vmax=100)
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'S1.png')
+
+    plt.imshow(scn['S2_an'], cmap='jet', vmin=0, vmax=100)
     plt.colorbar()
     plt.title('S2')
-    plt.show()
-    
-    
-    S1 = np.nan_to_num(scn['S1_an'].values)
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'S2.png')
+
     S2 = np.nan_to_num(scn['S2_an'].values)
     S3 = np.nan_to_num(scn['S3_an'].values)
-    S4 = np.nan_to_num(scn['S4_an'].values)
     S5 = np.nan_to_num(scn['S5_an'].values)
-    S6 = np.nan_to_num(scn['S6_an'].values)
     CAN = np.nan_to_num(scn['confidence_an'].values)
 
-#create some false colour plots
-    
-# this does more natural colours    
-#    green = norm(S1)
-#    red = norm(S2)
-#    IR = norm(S3 + S4 + S5 + S6)
-#    blue = norm(0.8 * green - 0.1 * red - 0.1 * IR)
+    # create some false colour plots
 
-    
-    red = norm(S5) #1.6
-    green = norm(S3) #.87
-    blue = norm(S2) # .67
+    # this does more natural colours
+    #    green = norm(S1)
+    #    red = norm(S2)
+    #    IR = norm(S3 + S4 + S5 + S6)
+    #    blue = norm(0.8 * green - 0.1 * red - 0.1 * IR)
+
+    red = norm(S5)  # 1.6
+    green = norm(S3)  # .87
+    blue = norm(S2)  # .67
 
     rgb = np.dstack((red, green, blue))
-    brightness=0.2
+    brightness = 0.2
     hsv = mcolors.rgb_to_hsv(rgb)
     hsv[:, :, 2] += brightness
     rgb = mcolors.hsv_to_rgb(hsv)
     rgb[rgb > 1] = 1
-    print('here a')
+
     LatPos = str(round(np.array(scn['latitude_an'].values)[0, 0], 6))
     LonPos = str(round(np.array(scn['longitude_an'].values)[0, 0], 6))
-    FileStr='xx'
+    FileStr = 'xx'
     TitleStr = '(' + LatPos + ', ' + LonPos + ')\n' + FileStr
-    #   if plot is True:
-    plt.figure()
-    print('here b')
-    plt.imshow(rgb)
-    
-    plt.title('False colour image\n' + TitleStr)
-    plt.show()
 
-#now mask it    
-    #plt.subplot(1,2,2)
+    plt.figure()
+    plt.imshow(rgb)
+    plt.title('False colour image\n' + TitleStr)
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'False_Color.png')
+
+    # now mask it
     if mask is not None:
         mask = mask.astype('bool')
         red[mask] = 254 / 255
         green[mask] = 253 / 255
         blue[mask] = 185 / 255
-    
+
     rgb = np.dstack((red, green, blue))
-    brightness=0.2
+    brightness = 0.2
     hsv = mcolors.rgb_to_hsv(rgb)
     hsv[:, :, 2] += brightness
     rgb = mcolors.hsv_to_rgb(hsv)
     rgb[rgb > 1] = 1
-    print('here a')
+
     LatPos = str(round(np.array(scn['latitude_an'].values)[0, 0], 6))
     LonPos = str(round(np.array(scn['longitude_an'].values)[0, 0], 6))
-    FileStr='xx'
+    FileStr = 'xx'
     TitleStr = '(' + LatPos + ', ' + LonPos + ')\n' + FileStr
-    #   if plot is True:
+
     plt.imshow(rgb)
     plt.title('False colour image masked\n' + TitleStr)
-    plt.show()
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'False_Color_Masked.png')
 
-    # now plot snow mask
-    #0.4 thresholf
-    ndsi=(S2-S5)/(S2+S5)
+    # now plot Normalised Difference Snow Index
+    ndsi = (S2-S5)/(S2+S5)
 
     plt.figure()
-    print('here b')
     plt.imshow(ndsi)
     plt.colorbar()
     plt.title('NDSI image\n' + TitleStr)
-    plt.show()
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'NDSI.png')
 
-    
     bitmeanings = {
             'Coastline': 1,
             'Ocean': 2,
@@ -126,28 +137,44 @@ def plot_slstr(filenames,mask):
             'Twilight': 2048,
             'NDSI snow': 8192}
 
-#plot the masks
-
-    mask_ocean= CAN & 2
+    # Plot the masks
+    mask_ocean = CAN & bitmeanings['Ocean']
     plt.imshow(mask_ocean)
     plt.title('mask ocean\n')
-    plt.show()
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'Mask_Ocean.png')
 
-
-    mask_dryland= CAN & 24
+    mask_dryland = CAN & bitmeanings['Dry land']
     plt.imshow(mask_dryland)
     plt.title('mask dryland\n')
-    plt.show()
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'False_DryLand.png')
 
-
-    mask_coastline= CAN & 1
+    mask_coastline = CAN & bitmeanings['Coastline']
     plt.imshow(mask_coastline)
     plt.title('mask coastline\n')
-    plt.show()
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'False_Coast.png')
 
-
-    mask_ndsi= CAN & 8192
+    mask_ndsi = CAN & bitmeanings['NDSI snow']
     plt.imshow(mask_ndsi)
     plt.title('mask NSDI\n')
-    plt.show() 
-    
+    if show:
+        plt.show()
+    if save:
+        plt.savefig(odir + 'False_NDSI.png')
+
+
+if __name__ == "__main__":
+    from glob import glob
+    import sys
+
+    indir = sys.argv[1]
+    filenames = glob(indir + '/*')
+    plot_slstr(filenames, save=True, odir='/home/proud/Desktop/')
