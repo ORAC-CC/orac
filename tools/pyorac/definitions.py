7fef087487a1c6b29225da7f69d3cddf1bf4a9d0
Adam Povey <adam.povey@physics.ox.ac.uk>
2022-07-13 17:24:00 +0100

    Not yet ready to make the netCDF LUTs the only option

--- tools/pyorac/definitions.py
+++ tools/pyorac/definitions.py
@@ -632,10 +632,12 @@ class ParticleType:
 
 
 # Using non-imager LUTs and Baum properties at Greg's recommendation
-SETTINGS = {'WAT': ParticleType("liquid-water", sad='netcdf'),
-            'ICE': ParticleType("water-ice", sad='netcdf', microphysical_model='agg')}
+SETTINGS = {'WAT': ParticleType("WAT", sad="WAT"),
+            'ICE': ParticleType("ICE", sad="ICE_baum")}
 
 # Uncomment to use new netcdf LUTs
+#SETTINGS = {'WAT': ParticleType("liquid-water", sad='netcdf'),
+#            'ICE': ParticleType("water-ice", sad='netcdf', microphysical_model='agg')}
 
 
 tau = Invpar('ITau', ap=-1.0, sx=1.5)
