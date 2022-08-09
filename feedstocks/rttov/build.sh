e1e12d43f368529581b526db45ebbc3d2c533633
Adam Povey <adam.povey@physics.ox.ac.uk>
2022-08-09 10:52:17 +0100

    Update RTTOV build
    - Bump to v13.1
    - Switch to using conda-forge version of netcdf-fortran (or, at least,
      a copy that's stored in the normal library folder)
    - Copy the pyRTTOV files over as a first go at installing that
    - Patch a file that compiles very slowly with new GNU versions

--- feedstocks/rttov/build.sh
+++ feedstocks/rttov/build.sh
@@ -12,3 +12,7 @@ REL_PATH=`python -c "import os.path; print(os.path.relpath('$PREFIX', '$PWD'))"`
 cd src
 ../build/Makefile.PL RTTOV_HDF=1 RTTOV_F2PY=1 RTTOV_USER_LAPACK=1
 make ARCH=$ARCH INSTALLDIR=$REL_PATH
+
+# Extremely lazy installation of pyRTTOV
+# NOTE: should do something like HDF5 and make multiple outs to minimise builds
+cp -r ../wrapper/pyrttov ${PREFIX}/lib/
