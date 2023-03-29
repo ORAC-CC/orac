68a35dd809d5bbe64583cdb3c477cb51cc65c512
Adam Povey <adam.povey@physics.ox.ac.uk>
2023-03-29 11:11:44 +0100

    Set Invpar.ap and .fg to None when not provided

--- tools/pyorac/definitions.py
+++ tools/pyorac/definitions.py
@@ -521,14 +521,12 @@ class Invpar:
 
     def __init__(self, var, ap=None, fg=None, sx=None):
         self.var = var
-        if ap:
-            self.ap = ap
-            if fg is None:
-                self.fg = ap
-        if fg:
+        self.ap = ap
+        if fg is None:
+            self.fg = ap
+        else:
             self.fg = fg
-        if sx:
-            self.sx = sx
+        self.sx = sx
 
     def driver(self):
         """Output lines for a driver file to specify these settings"""
